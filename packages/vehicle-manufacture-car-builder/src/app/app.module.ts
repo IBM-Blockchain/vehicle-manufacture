5cfc09309071f397554ff7ca7b3157177c55d48f
awjh-ibm <andrew.hurt1@ibm.com>
2018-05-25 14:07:45 +0100

    updated images
    Signed-off-by: awjh-ibm <andrew.hurt1@ibm.com>

---
+++ packages/vehicle-manufacture-car-builder/src/app/app.module.ts
@@ -0,0 +1,69 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Geolocation } from '@ionic-native/geolocation';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+import { HttpModule } from '@angular/http';
+
+import { MyApp } from './app.component';
+import { LoginPage } from '../pages/login/login';
+import { DesignerPage } from '../pages/designer/designer';
+import { BuilderPage } from '../pages/builder/builder';
+import { StatusPage } from '../pages/status/status';
+import { CarSlideComponent } from '../components/car-slide/car-slide';
+import { HeaderComponent } from '../components/header/header';
+
+// This needs to be changed if interacting with Ionic services - https://docs.ionic.io/setup.html
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': ''
+  }
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    LoginPage,
+    DesignerPage,
+    BuilderPage,
+    StatusPage,
+    CarSlideComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    LoginPage,
+    DesignerPage,
+    BuilderPage,
+    StatusPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Geolocation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ packages/vehicle-manufacture-car-builder/src/service-worker.js
@@ -0,0 +1,43 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/**
+ * Check out https://googlechrome.github.io/sw-toolbox/docs/master/index.html for
+ * more info on how to use sw-toolbox to custom configure your service worker.
+ */
+
+
+'use strict';
+importScripts('./build/sw-toolbox.js');
+
+self.toolbox.options.cache = {
+  name: 'ionic-cache'
+};
+
+// pre-cache our key assets
+self.toolbox.precache(
+  [
+    './build/main.js',
+    './build/main.css',
+    './build/polyfills.js',
+    'index.html',
+    'manifest.json'
+  ]
+);
+
+// dynamically cache any other local assets
+self.toolbox.router.any('/*', self.toolbox.cacheFirst);
+
+// for any other requests go to the network, cache,
+// and then only use that cached resource if your user goes offline
+self.toolbox.router.default = self.toolbox.networkFirst;
