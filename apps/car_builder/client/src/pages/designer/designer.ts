c75bdf94f2faedfa3c524820673269a66881049a
awjh-ibm <andrew.hurt1@ibm.com>
2019-06-03 09:49:10 +0100

    make car chooser better fit larger screens
    Signed-off-by: awjh-ibm <andrew.hurt1@ibm.com>

--- apps/car_builder/client/src/pages/designer/designer.ts
+++ apps/car_builder/client/src/pages/designer/designer.ts
@@ -11,15 +11,17 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-designer',
   templateUrl: 'designer.html'
 })
-export class DesignerPage {
+export class DesignerPage implements OnInit {
   cars: Object[];
+
+  private numSlides = 1;
 
   constructor(public navController: NavController) {
     this.cars = [{
@@ -43,4 +45,18 @@ export class DesignerPage {
     }]
   }
 
+  ngOnInit() {
+    this.onResize({target: window });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.numSlides = Math.floor(event.target.innerWidth / 300);
+
+    if (this.numSlides < 1) {
+      this.numSlides = 1;
+    } else if (this.numSlides > this.cars.length) {
+      this.numSlides = this.cars.length;
+    }
+  }
 }
