be67b1cef0f8806bc500c569a6fe32352a8aa4d8
Liam Grace <liamgrace.896@gmail.com>
2019-06-07 11:28:17 +0100

    Added licenses to files

--- contract/src/constants.ts
+++ contract/src/constants.ts
@@ -1,6 +1,17 @@
 /*
-SPDX-License-Identifier: Apache-2.0
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
+
 
 export const NetworkName = 'org.acme.vehicle_network';
 export const NetworkNameUUID = '01b1625d-c51f-4ffb-8b8c-d206eec1207d';
